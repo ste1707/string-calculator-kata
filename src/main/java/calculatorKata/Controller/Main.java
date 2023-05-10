6f1616ae77ef3539f02c788bffa38f829f302e38
ste1707 <stefanodraganoiu35@gmail.com>
2023-05-10 19:41:21 +0200

    step4

--- src/main/java/calculatorKata/Controller/Main.java
+++ src/main/java/calculatorKata/Controller/Main.java
@@ -28,6 +28,22 @@ public class Main {
 		
 		
 		if(numbers.getNumeri().length() > 1) {
+			if(numbers.getNumeri().charAt(0) == '/' && numbers.getNumeri().charAt(1) == '/') {
+				
+				char delimitatore = numbers.getNumeri().charAt(2);
+				
+				int somma = 0;
+				String[] numeriString = numbers.getNumeri().split("(?<=^.{3})");
+			
+				for (int i = 1; i < numeriString.length; i++) {
+					String[] numeriString2 = numeriString[i].split("[,\n"+delimitatore+"]");
+					for (String string : numeriString2) {
+						somma += Integer.parseInt(string);
+					}					
+				}
+				Somma s = new Somma(somma);
+				return s;
+			}
 			int somma = 0;
 			String[] numeriString = numbers.getNumeri().split("[,\n]");
 			for (String string : numeriString) {
